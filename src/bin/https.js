89fb5fa8c14207fb9b76cd9646e959a71d71f3c2
Brandon Kelley <brandonmkelley@outlook.com>
2019-10-23 00:43:04 +0000

    Testing automatic SSL deploy

--- src/bin/https.js
+++ src/bin/https.js
@@ -6,20 +6,24 @@
 
 var app = require('../app');
 var debug = require('debug')('userportal-web:server');
-var http = require('http');
+var https = require('https');
+var fs = require('fs');
 
 /**
  * Get port from environment and store in Express.
  */
 
-var port = normalizePort(process.env.PORT || '3000');
+var port = normalizePort(process.env.PORT || '443');
 app.set('port', port);
 
 /**
  * Create HTTP server.
  */
 
-var server = http.createServer(app);
+var server = https.createServer({
+  key: fs.readFileSync('ssl-key.pem'),
+  cert: fs.readFileSync('ssl-cert.pem')
+});
 
 /**
  * Listen on provided port, on all network interfaces.
