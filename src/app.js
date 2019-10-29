0f5f9d580e1fa55f5bc30e60c2ea37fa361eb60d
Brandon Kelley <brandonmkelley@outlook.com>
2019-10-29 22:33:29 +0000

    Successfully integrated socket.io

--- src/app.js
+++ src/app.js
@@ -1,60 +1,78 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-// Firebase App (the core Firebase SDK) is always required and
-// must be listed before other Firebase SDKs
-var firebase = require("firebase/app");
-
-// Add the Firebase products that you want to use
-require("firebase/auth");
-
-var firebaseConfig = require('./bin/firebaseconfig.js');
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-app.set('firebase', firebase);
-
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+module.exports = (app, server) => {
+  
+  var express = require('express');
+  var createError = require('http-errors');
+  var path = require('path');
+  var cookieParser = require('cookie-parser');
+  var bodyParser = require('body-parser');
+  var logger = require('morgan');
+  
+  var indexRouter = require('./routes/index');
+  var usersRouter = require('./routes/users');
+  
+  // view engine setup
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'ejs');
+  
+  app.use(logger('dev'));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(express.static(path.join(__dirname, 'public')));
+  
+  // Firebase App (the core Firebase SDK) is always required and
+  // must be listed before other Firebase SDKs
+  var firebase = require("firebase/app");
+  
+  // Add the Firebase products that you want to use
+  require("firebase/auth");
+  
+  var firebaseConfig = require('./bin/firebaseconfig.js');
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  app.set('firebase', firebase);
+  
+  var io = require('socket.io')(server);
+  app.set('io', io);
+  
+  io.on('connection', socket => {
+      socket.on('sign-in-request', data => {
+          
+        firebase.auth().createUserWithEmailAndPassword(data.email, data.password)
+          .then(result => {
+              if (result) {
+                  console.log(result);
+                  socket.emit('sign-in-success', result);
+              }
+          })
+          .catch(error => {
+              console.log(error);
+              socket.emit('sign-in-failure', error);
+          });
+      });
+  });
+  
+  app.use('/', indexRouter);
+  app.use('/users', usersRouter);
+  
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+    next(createError(404));
+  });
+  
+  // error handler
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+};
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,3 +1,4 @@
+
 var express = require('express');
 var router = express.Router();
 
@@ -10,17 +11,7 @@ router.post('/', (req, res, next) => {
     console.log(req.body);
     var firebase = req.app.get('firebase');
     
-    firebase.auth().createUserWithEmailAndPassword(req.body.email, req.body.password)
-        .then(result => {
-            if (result) {
-                console.log(result);
-                res.end(JSON.stringify({ result: result }));
-            }
-        })
-        .catch(function(error) {
-            console.log(error);
-            res.end(JSON.stringify({ error: error }));
-        });
+    
 });
 
 module.exports = router;
