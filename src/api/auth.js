4b764e50c857e504d180afc171f317c4b4bae74b
Brandon Kelley <brandonmkelley@outlook.com>
2019-12-11 02:00:19 +0000

    Adding client and server side serialization.

--- src/api/auth.js
+++ src/api/auth.js
@@ -6,24 +6,27 @@ module.exports = (app) => {
   
   io.on('connection', socket => {
         socket.on('sign-up-request', data => {
-            firebase.auth().createUserWithEmailAndPassword(data.email, data.password)
-              .then(result => socket.emit('sign-up-success', result))
-              .catch(error => socket.emit('sign-up-failure', error));
+            console.log('hello sign up');
+            var creds = JSON.parse(data);
+            firebase.auth().createUserWithEmailAndPassword(creds.email, creds.password)
+              .then(result => socket.emit('sign-up-success', JSON.stringify(result)))
+              .catch(error => socket.emit('sign-up-failure', JSON.stringify(error)));
         });
       
       socket.on('log-in-request', data => {
-          firebase.auth().signInWithEmailAndPassword(data.email, data.password)
-            .then(result => socket.emit('log-in-success', result))
-            .catch(error => socket.emit('log-in-failure', error));
+          var creds = JSON.parse(data);
+          firebase.auth().signInWithEmailAndPassword(creds.email, creds.password)
+            .then(result => socket.emit('log-in-success', JSON.stringify(result)))
+            .catch(error => socket.emit('log-in-failure', JSON.stringify(error)));
       });
       
       socket.on('log-out-request', data => {
           firebase.auth().signOut()
-            .then(result => socket.emit('log-out-success', result))
-            .catch(error => socket.emit('log-out-failure', error));
+            .then(result => socket.emit('log-out-success', JSON.stringify(result)))
+            .catch(error => socket.emit('log-out-failure', JSON.stringify(error)));
       });
       
-      firebase.auth().onAuthStateChanged(data => socket.emit('auth-state-changed', data));
+      firebase.auth().onAuthStateChanged(data => socket.emit('auth-state-changed', JSON.stringify(data)));
   });
     
 };
